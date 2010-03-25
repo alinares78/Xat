fd31913cab5c569c606b38b242bad55de28e5896
unknown <ies@.(none)>
2010-03-25 17:30:32 +0100

    El cliente se deconecta con disconnect, envia mail

--- XatClient/Main.cs
+++ XatClient/Main.cs
@@ -5,85 +5,105 @@ using System.IO;
 
 namespace XatClient
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
             // Faig un objecte de la classe client li paso la ip del servidor del cole
             // Esta es mi ip
-			Client client = new Client("192.168.130.81", 9898);
-			
-			if (client.ConnectToServer())
-			{
-				while (true)
-				{
+            Client client = new Client("192.168.130.81", 9898);
+            Boolean desconectar = true;
+
+            if (client.ConnectToServer())
+            {
+                while ((true) && desconectar == true)
+                {
                     try
                     {
                         //Aqui el cliente envia todo el rato la misma frase
                         //client.WriteLine("Hola! Sóc el client enviant un missatge!");
                         //Con el siguiente codigo el cliente escribira por teclado y el servidor verá todo lo que vea
-                        //Lo que el cliente escriba que la consola lo lea
-                        client.WriteLine(Console.ReadLine());
+                        //Lo que el cliente escriba que la consola lo lea:Jucto la linea de abajo
+                        //client.WriteLine(Console.ReadLine());
+
+                        //Aqui comprobamos que cuando el cliente escriba disconnect se desconecte del servidor
+                        //y como el while(true) hace como un bucle infinito en el momento que lea disconnect se desconectará
+                        //declararé una variable tipo string para que cuando entre por primera vez escriba algo y k luego lo compruebe
+
+                        String cadena = Console.ReadLine();
+                        if (cadena == "disconnect")
+                        {
+                            desconectar = false;
+                        }
+                        //Aqui envio el mail y lo que leo seguido
+                        //client.WriteLine(mail);
+                        client.WriteLine(cadena);
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine("S'ha desconectat el servidor");
                         Console.ReadKey(true);
                     }
-                        
                 }
-			}
-		}
-	}
-	
-	public class Client
-	{
-		private NetworkStream netStream;
-		private StreamReader readerStream;
-		private StreamWriter writerStream;
-		private IPEndPoint server_endpoint;
-		private TcpClient tcpClient;
-		
-		public Client(string ip, int port)
-		{
-			IPAddress address = IPAddress.Parse(ip);
-            // Lloc per agrupar la ip i el port conjuntament
-			server_endpoint = new IPEndPoint(address, port);
-		}
 
-		public string ReadLine()
-		{
-			return readerStream.ReadLine();
-		}
-		
-		public void WriteLine(string str)
-		{
-			writerStream.WriteLine(str);
-			writerStream.Flush();
-		}
-		
-		public bool ConnectToServer()
-		{
-			try
-			{
-				// tcpClient = new TcpClient(server_endpoint);
+            }
+        }
+    }
+
+    public class Client
+    {
+        private NetworkStream netStream;
+        private StreamReader readerStream;
+        private StreamWriter writerStream;
+        private IPEndPoint server_endpoint;
+        private TcpClient tcpClient;
+        string mail;
+        //Declaramos el constructor
+        public Client(string ip, int port)
+        {
+            IPAddress address = IPAddress.Parse(ip);
+            // Lloc per agrupar la ip i el port conjuntament
+            server_endpoint = new IPEndPoint(address, port);
+            mail = "araceli.linares78@gamil.com";
+        }
+
+        public string ReadLine()
+        {
+            return readerStream.ReadLine();
+        }
+
+        public void WriteLine(string str)
+        {
+            //Aqui envio el mail y lo que leo seguido
+            //client.WriteLine(mail);
+            writerStream.WriteLine(mail);
+            //Arriba en el main llamo a la funcio WriteLine y le paso x parámetro cadena
+            writerStream.WriteLine(str);
+            writerStream.Flush();
+        }
+
+        public bool ConnectToServer()
+        {
+            try
+            {
+                // tcpClient = new TcpClient(server_endpoint);
                 // Aqui pongo la ip donde me quiero conectar
                 tcpClient = new TcpClient("192.168.130.75", 9898);
-				
-				netStream = tcpClient.GetStream();
-				readerStream = new StreamReader(netStream);
-				writerStream = new StreamWriter(netStream);
-			}
-			catch(Exception e)
-			{
-				Console.WriteLine(e.StackTrace);
-				Console.WriteLine(e.Message);
-				return false;
-			}
 
-			Console.WriteLine("M'he connectat amb el servidor");
+                netStream = tcpClient.GetStream();
+                readerStream = new StreamReader(netStream);
+                writerStream = new StreamWriter(netStream);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.Message);
+                return false;
+            }
 
-			return true;		
-		}
-	}
+            Console.WriteLine("M'he connectat amb el servidor");
+
+            return true;
+        }
+    }
 }
--- XatServer/XatServer/Main.cs
+++ XatServer/XatServer/Main.cs
@@ -5,27 +5,29 @@ using System.IO;
 
 namespace XatServer
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hola, sóc el servidor!");
-			
-			Server servidor = new Server("192.168.130.81", 6969);
-			
-			if (!servidor.Start())
-			{
-				Console.WriteLine("No puc engegar el servidor!");
-			}
-			
-			if (servidor.WaitForAClient())
-			{
-				// Escribim tot el que ens envii el client
-				while (true)
-				{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, sóc el servidor!");
+
+            Server servidor = new Server("192.168.130.81", 6969);
+
+            if (!servidor.Start())
+            {
+                Console.WriteLine("No puc engegar el servidor!");
+            }
+
+            if (servidor.WaitForAClient())
+            {
+                // Escribim tot el que ens envii el client
+                while (true)
+                {
                     try
                     {
-                        Console.WriteLine("El client diu: " + servidor.ReadLine());
+                        Console.WriteLine(servidor.ReadLine() + " diu " + servidor.ReadLine());
+                        //Con servidor.ReadLine() al principio y servidor.ReadLine() al final me mostrará el mail del usuario y lo k escriba
+                        //jordigc.1988@gmail.com diu hola
                     }
                     catch (Exception ex)
                     {
@@ -33,86 +35,86 @@ namespace XatServer
                         // Volvemos a llamar al metodo para que cuando un cliente se desconecte no se salga y siga escuhando el servidor
                         servidor.WaitForAClient();
                     }
-					
-				}
-				
-				// server.WriteLine("Hi!"); 
-			}
-		}
-	}
-	
-	public class Server
-	{
-		private NetworkStream netStream;
-		private StreamReader readerStream;
-		private StreamWriter writerStream;
-		private IPEndPoint server_endpoint;
-		private TcpListener listener;
-		//Constructor del Server li pasem la ip i un port
-		public Server(string ip, int port)
-		{
-			IPAddress address = IPAddress.Parse(ip);
-			server_endpoint = new IPEndPoint(address, port);
-		}
-		
-		public string ReadLine()
-		{
-			return readerStream.ReadLine();
-		}
-		
-		public void WriteLine(string str)
-		{
-			writerStream.WriteLine(str);
-			writerStream.Flush();
-		}
-		
-		public bool Start()
-		{
-			try
-			{
-//				listener = new TcpListener(server_endpoint);
-                //Creem el TcpListener que es una classe que serveix per escoltar
-				listener = new TcpListener(9898);
 
-				listener.Start(); //start server
-			}
-			catch(Exception e)
-			{
-				Console.WriteLine(e.StackTrace);
-				return false;
-			}
-		
-			Console.WriteLine("Servidor engegat, escoltant a {0}:{1}", server_endpoint.Address, server_endpoint.Port);
-			
-			return true;
-		}
-		
-		public bool WaitForAClient()
-		{
+                }
+
+                // server.WriteLine("Hi!"); 
+            }
+        }
+    }
+
+    public class Server
+    {
+        private NetworkStream netStream;
+        private StreamReader readerStream;
+        private StreamWriter writerStream;
+        private IPEndPoint server_endpoint;
+        private TcpListener listener;
+        //Constructor del Server li pasem la ip i un port
+        public Server(string ip, int port)
+        {
+            IPAddress address = IPAddress.Parse(ip);
+            server_endpoint = new IPEndPoint(address, port);
+        }
+
+        public string ReadLine()
+        {
+            return readerStream.ReadLine();
+        }
+
+        public void WriteLine(string str)
+        {
+            writerStream.WriteLine(str);
+            writerStream.Flush();
+        }
+
+        public bool Start()
+        {
+            try
+            {
+                //				listener = new TcpListener(server_endpoint);
+                //Creem el TcpListener que es una classe que serveix per escoltar
+                listener = new TcpListener(9898);
+
+                listener.Start(); //start server
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+
+            Console.WriteLine("Servidor engegat, escoltant a {0}:{1}", server_endpoint.Address, server_endpoint.Port);
+
+            return true;
+        }
+
+        public bool WaitForAClient()
+        {
             // AcceptSocket : Esperem una connexio d'un client
             // Esperarà fins que es conecti un client
-			Socket serverSocket = listener.AcceptSocket();
-			
-			try
-			{
-				if (serverSocket.Connected)
-				{
-                    // Aquí creem un buffer(Stream fluxe de dades) per poder crear un writerStream i un readerStream
-					netStream = new NetworkStream(serverSocket);
-					// Aquí podem llegir o escriure coses
-					writerStream = new StreamWriter(netStream);
-					readerStream = new StreamReader(netStream);
-				}
-			}
-			catch(Exception e)
-			{
-				Console.WriteLine(e.StackTrace);
-				return false;
-			}
+            Socket serverSocket = listener.AcceptSocket();
 
-			Console.WriteLine("Un client s'ha connectat!");
-					
-			return true;
-		}
-	}
+            try
+            {
+                if (serverSocket.Connected)
+                {
+                    // Aquí creem un buffer(Stream fluxe de dades) per poder crear un writerStream i un readerStream
+                    netStream = new NetworkStream(serverSocket);
+                    // Aquí podem llegir o escriure coses
+                    writerStream = new StreamWriter(netStream);
+                    readerStream = new StreamReader(netStream);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+
+            Console.WriteLine("Un client s'ha connectat!");
+
+            return true;
+        }
+    }
 }
